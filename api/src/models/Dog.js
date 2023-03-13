cf46ddf77159f163f541a4bd6c53321caf5444a7
Renzo Viscio <nviscio@gmail.com>
2023-03-13 20:46:37 -0300

    Models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Dog",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      lifeExpectancy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
